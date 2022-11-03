9e11e03d738f1a51dc6b1e47a08095ed81e14b96
Mohamed Sami <srmf2025@gmail.com>
2022-11-03 21:34:56 +0530

    :sparkles: add javascript starter with TODOs

---
+++ index.js
@@ -0,0 +1,27 @@
+// TODO: select the input with class task-input
+// TODO: select the button with class task-add-btn
+// TODO: select the div with class task-elements
+
+let taskCount = 1;
+
+function addTask() {
+  taskId = `task-${++taskCount}`;
+
+  // TODO: create <div class="task-element"></div>
+
+  // TODO: create <input class="task-checkbox" type="checkbox" id=taskId />
+
+  // TODO: create <label for=taskId>task</label>
+
+  // TODO: add taskCheckbox & taskLabel to <div class="task-element"></div>
+
+  // TODO add <div class="task-element">...</div> to <div class="task-elements">...</div>
+}
+
+// TODO: set a task as complete by crossing out the label by including task-done class
+function setTaskComplete() {}
+
+// TODO: add event listener to task-add-btn
+
+// TODO: add event listener to task-checkboxes
+function addListenerToCheckboxes() {}
